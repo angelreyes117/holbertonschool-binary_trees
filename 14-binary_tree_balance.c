5dc18ba46679f742235dab8685f8550305264275
Diego Esquivel <desquimor@Hogwarts>
2025-01-02 19:01:23 -0600

    fix 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+    left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+    right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
-	return (left_height > right_height ? left_height : right_height);
+    return (left_height > right_height ? left_height : right_height);
 }
 
 /**
@@ -27,9 +27,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	return (0);
+    if (tree == NULL)
+        return (0);
 
-	return ((int)(binary_tree_height(tree->left)
-			- binary_tree_height(tree->right)));
+    int left_height = (int)binary_tree_height(tree->left);
+    int right_height = (int)binary_tree_height(tree->right);
+
+    return (left_height - right_height);
 }
