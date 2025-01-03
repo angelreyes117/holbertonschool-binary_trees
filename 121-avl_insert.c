4f1fdeeabf0857df853a3d8f866f02bc7777fb9b
Diego Esquivel <desquimor@Hogwarts>
2025-01-03 11:29:28 -0600

    fix betty 121-avl_insert.c

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -2,6 +2,53 @@
 #include <stdlib.h>
 
 /**
+ * avl_insert_node - Helper to insert a value into an AVL tree
+ * @tree: Double pointer to the root node of the AVL tree
+ * @value: Value to insert in the tree
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+avl_t *avl_insert_node(avl_t **tree, int value)
+{
+	avl_t *new_node;
+
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			new_node = binary_tree_node(*tree, value);
+			if (!new_node)
+				return (NULL);
+			(*tree)->left = new_node;
+		}
+		else
+		{
+			new_node = avl_insert_node(&((*tree)->left), value);
+		}
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			new_node = binary_tree_node(*tree, value);
+		if (!new_node)
+			return (NULL);
+		(*tree)->right = new_node;
+		}
+		else
+		{
+			new_node = avl_insert_node(&((*tree)->right), value);
+		}
+	}
+	else
+	{
+		return (NULL); /* Value already exists */
+	}
+
+	return (new_node);
+}
+
+/**
  * balance_tree - Balances an AVL tree after insertion
  * @tree: Double pointer to the root node of the tree
  *
@@ -49,45 +96,15 @@ avl_t *avl_insert(avl_t **tree, int value)
 	if (*tree == NULL)
 	{
 		new_node = binary_tree_node(NULL, value);
-		if (new_node == NULL)
-			return (NULL);
-		*tree = new_node;
-		return (new_node);
+	if (new_node == NULL)
+		return (NULL);
+	*tree = new_node;
+	return (new_node);
 	}
 
-	if (value < (*tree)->n)
-	{
-		if ((*tree)->left == NULL)
-		{
-			new_node = binary_tree_node(*tree, value);
-			if (!new_node)
-				return (NULL);
-			(*tree)->left = new_node;
-		}
-		else
-		{
-			new_node = avl_insert(&((*tree)->left), value);
-		}
-	}
-	else if (value > (*tree)->n)
-	{
-		if ((*tree)->right == NULL)
-		{
-			new_node = binary_tree_node(*tree, value);
-			if (!new_node)
-				return (NULL);
-			(*tree)->right = new_node;
-		}
-		else
-		{
-			new_node = avl_insert(&((*tree)->right), value);
-		}
-	}
-	else
-	{
-		return (NULL); /* Value already exists */
-	}
+	new_node = avl_insert_node(tree, value);
+	if (new_node)
+		*tree = balance_tree(tree);
 
-	*tree = balance_tree(tree);
 	return (new_node);
 }
